621c7dfbf6e13c7054f373712753f13897de5cb1
Leevi Graham <leevi@newism.com.au>
2010-07-07 14:03:43 +1000

    Added fieldtype settings

---
+++ views/fieldtype/settings.php
@@ -0,0 +1,11 @@
+<div class="tg">
+	<h2>Field Styling</h2>
+	<table>
+		<tbody>
+			<tr>
+				<th scope="row">CSS</th>
+				<td><textarea name="<?= $input_prefix."[css]" ?>"><?= $css ?></textarea></td>
+			</tr>
+		</tbody>
+	</table>
+</div>

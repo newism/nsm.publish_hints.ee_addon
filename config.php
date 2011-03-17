a20704fda0fd606fd0c77a8a1cfd9679fd9740e5
Leevi Graham <leevi@newism.com.au>
2011-03-18 00:16:30 +1100

    Update links to ee-garage.com

--- config.php
+++ config.php
@@ -8,11 +8,11 @@
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
  * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
- * @link			http://expressionengine-addons.com/nsm-publish-hints
- * @see				http://expressionengine-addons.com/nsm-addon-updater
+ * @link			http://ee-garage.com/nsm-publish-hints
+ * @see				http://ee-garage.com/nsm-addon-updater
  */
 
 $config['name'] 	= "NSM Publish Hints";
 $config["version"] 	= "1.0.0";
 
-$config['nsm_addon_updater']['versions_xml'] = 'http://expressionengine-addons.com/nsm-publish-hints/release-notes';
+$config['nsm_addon_updater']['versions_xml'] = 'http://ee-garage.com/nsm-publish-hints/release-notes/feed';

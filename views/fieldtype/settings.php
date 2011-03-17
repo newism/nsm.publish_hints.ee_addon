69a111db9c59d53bafabf30feae20de6b776e67c
Leevi Graham <leevi@newism.com.au>
2011-03-17 22:15:40 +1100

    Row styling

--- views/fieldtype/settings.php
+++ views/fieldtype/settings.php
@@ -4,7 +4,7 @@
 		<div class="alert info">Add your own specific styles. Alternatively update the theme css. Target the publish hits field with <code>class="nsm-publish-hints"</code></div>
 		<table>
 			<tbody>
-				<tr>
+				<tr class="even">
 					<th scope="row">CSS Styles</th>
 					<td>
 						<textarea name="<?= $input_prefix."[css]" ?>"><?= $css ?></textarea>

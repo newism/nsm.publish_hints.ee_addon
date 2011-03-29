457494041b5d0999f6bd1cae3c607be460368331
Leevi Graham <leevi@newism.com.au>
2011-03-30 09:06:41 +1100

    Updated version number and copyright date

--- config.php
+++ config.php
@@ -4,15 +4,15 @@
  * Config file for NSM Publish Hints
  *
  * @package			NsmPublishHints
- * @version			1.0.0
+ * @version			1.0.1
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
- * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
+ * @copyright 		Copyright (c) 2007-2011 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
  * @link			http://ee-garage.com/nsm-publish-hints
  * @see				http://ee-garage.com/nsm-addon-updater
  */
 
 $config['name'] 	= "NSM Publish Hints";
-$config["version"] 	= "1.0.0";
+$config["version"] 	= "1.0.1";
 
 $config['nsm_addon_updater']['versions_xml'] = 'http://ee-garage.com/nsm-publish-hints/release-notes/feed';
